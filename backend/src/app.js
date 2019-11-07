607c7deb3c390c2335d9c16b2f1dae19f7babf4b
przem <przemichal@gmail.com>
2019-11-07 13:49:25 +0100

    Connect mongodb atlas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+require('./database/database');
+
 const app = express();
 
 app.get('/example', (req, res) => {
---
+++ backend/src/database/database.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const config = require('../config/config');
+
+mongoose
+  .connect(config.MONGO_URI, {
+    dbName: config.MONGO_INITDB_DATABASE,
+    user: config.MONGO_INITDB_ROOT_USERNAME,
+    pass: config.MONGO_INITDB_ROOT_PASSWORD,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+mongoose.connection
+  .on('connected', () => {
+    console.log('database: connected');
+  })
+  .on('disconnected', () => {
+    console.log('database: disconnected');
+  })
+  .on('error', err => {
+    console.error(err);
+  })
+  .on('reconnected', () => {
+    console.log('database: reconnected');
+  });
