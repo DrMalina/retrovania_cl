4071607c2bcb1219c845ad5c54ed2a3b124bfaa7
przem <przemichal@gmail.com>
2019-11-07 19:05:59 +0100

    Update eslint config: set comma-dangle rule to error

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const app = require('./src/app');
 const config = require('./src/config/config');
 
-const PORT = config.PORT;
+const { PORT } = config;
 app.listen(PORT, () => {
   console.log(`up&running on ${PORT}.`);
 });
