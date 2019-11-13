d80852602a89c9b083e1490a55310ad9a6efb455
Kamil <37276273+kamilkulik@users.noreply.github.com>
2019-11-13 17:57:51 +0100

    [ND-22] Add user registration end point

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,45 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const users = require('./routes/users');
-
-require('./database/database');
+const routes = require('./routes/rootRoute');
 
 const app = express();
-const port = 3000;
 
-
-app.get('/example', (req, res) => {
-  res.status(200).send({ example: 'example' });
-});
-// using post method & request body to avoid exposing data
-// express post handler
-// body parser - by default express doesn't provide access to req body
-// hash the password
-// authentication flow (new user / registered), connection to the database - probably a separate file
-// redirect user & send token
-// middleware for handling token
-// Connecting to MongoDB. Uri required
-
-try {
-  mongoose.connect('mongodb://127.0.0.1:27017/User')
-    .then(() => console.log('Now connected to MongoDB'))
-    .catch(err => console.log('Something went wrong'));
-} catch(error) {
-  console.log(error);
-}
-
-
-app.use(express.json()); // This is a built-in middleware function in Express. It parses incoming requests with JSON payloads and is based on body-parser. A new body object containing the parsed data is populated on the request object after the middleware (i.e. req.body), or an empty object ({}) if there was no body to parse, the Content-Type was not matched, or an error occurred.
-app.use('/api/users', users); // API endpoint
-
-// Homepage added at route '/'
-app.get('/', (req, res) => {
-  res.status(200)
-    .send(`Hit home page`);
-});
-
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+app.use(express.json());
+app.use('/api', routes);
 
 module.exports = app;
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,37 +1,49 @@
 const mongoose = require('mongoose');
-const Joi = require('@hapi/joi')
+const Joi = require('@hapi/joi');
 
-const User = mongoose.model('User', new mongoose.Schema({
-
+const User = mongoose.model(
+  'User',
+  new mongoose.Schema({
     name: {
-        type: String,
-        required: true,
-        minlength: 5,
-        maxlength: 100
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 100,
     },
     email: {
-        type: String,
-        required: true,
-        maxlength: 255,
-        unique: true
+      type: String,
+      required: true,
+      maxlength: 255,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
-        maxlength: 1024
-    }
-}));
+      type: String,
+      required: true,
+      maxlength: 1024,
+    },
+  }),
+);
 
 // FUNCTION FOR VALIDATING REQUEST
 
 const validateUser = user => {
-    const schema = Joi.object({
-        name: Joi.string().min(5).max(100).required(),
-        email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(255).required(),
-    });
-    return schema.validate(user);
-}
+  const schema = Joi.object({
+    name: Joi.string()
+      .min(5)
+      .max(100)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required(),
+  });
+  return schema.validate(user);
+};
 
 exports.User = User;
 exports.validate = validateUser;
