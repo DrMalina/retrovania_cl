45bae3a507bd94f9603c59240f3f74dc5060ef13
Vitalik Pylypets <v.pylypets94@gmail.com>
2019-11-17 21:38:40 +0100

    [ND-43] Resolve imports in App and SignIn files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import { Header } from './components/Header/';
+import { Header } from 'components/Header/';
 
-import { Home } from './pages/Home';
-import { Games } from './pages/Games';
-import { SignIn } from './pages/SignIn';
-import { SignUp } from './pages/SignUp';
+import { Home } from 'pages/Home';
+import { Games } from 'pages/Games';
+import { SignIn } from 'pages/SignIn';
+import { SignUp } from 'pages/SignUp';
 
 import './App.css';
 
