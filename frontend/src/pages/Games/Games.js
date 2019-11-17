f6a38f17800b671dd20b6ce950a491b1a2e5d35c
dogrodnik <d.ogrodnik96@gmail.com>
2019-11-17 19:16:12 +0100

    [ND-28] Add simple games component

--- frontend/src/pages/Games/Games.js
+++ frontend/src/pages/Games/Games.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Game } from 'components/Game/';
+
+const exampleDescription =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+const exampleData = [
+  {
+    title: 'Super Mario Bros',
+    year: 1985,
+    company: 'Nintendo',
+    description: exampleDescription
+  },
+  {
+    title: 'Contra',
+    year: 1988,
+    company: 'Konami',
+    description: exampleDescription
+  },
+  {
+    title: 'Battle City',
+    year: 1985,
+    company: 'Namco',
+    description: exampleDescription
+  }
+];
 
 const Games = () => {
-  return <h2>Games</h2>;
+  const renderGames = data => {
+    return data.map(({ company, description, title, year }) => {
+      return (
+        <Game
+          company={company}
+          description={description}
+          key={title + company}
+          title={title}
+          year={year}
+        />
+      );
+    });
+  };
+  return <div>{renderGames(exampleData)}</div>;
 };
 
 export { Games };
