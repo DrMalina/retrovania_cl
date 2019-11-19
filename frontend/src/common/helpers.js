55aed05a0cbacdc56d3dbc92fb5ea93044a12a9a
Maciej Malinowski <maciej.malinowski7@gmail.com>
2019-11-19 15:40:58 +0100

    [ND-20] Move rendering fields  part to helpers.js

--- frontend/src/common/helpers.js
+++ frontend/src/common/helpers.js
@@ -1,10 +1,20 @@
-/*  */
-// A file for helper functions
-/*  */
+import React from 'react';
+import { Input } from 'components/Input';
 
 //e.g. 'confirmPassword' => 'Confirm Password'
 export const capitalizeFirstLetter = string => {
   return string
     .replace(/([A-Z])/g, ' $1') // insert a space before all caps
     .replace(/^./, str => str.toUpperCase()); // capitalize first letter
+};
+
+export const renderFormFields = values => {
+  return values.map((el, index) => (
+    <Input
+      key={index}
+      label={capitalizeFirstLetter(el)}
+      name={el}
+      type={el === 'confirmPassword' || el === 'password' ? 'password' : 'text'}
+    />
+  ));
 };
