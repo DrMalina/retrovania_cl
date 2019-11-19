ae4384c133f3465ef0a235d53ee7705a140dd788
przem <przemichal@gmail.com>
2019-11-20 00:56:17 +0100

    [ND-45] Install required dependencies (redux, react-redux)

---
+++ frontend/src/redux/placeholder/placeholderActions.js
@@ -0,0 +1,13 @@
+// utilities:
+import * as messages from 'redux/placeholder/placeholderMessages';
+
+export const placeholderCleanup = () => ({
+  type: messages.PLACEHOLDER_CLEANUP,
+});
+
+export const placeholderChangeExample = (newExample) => ({
+  type: messages.PLACEHOLDER_CHANGE_EXAMPLE,
+  payload: {
+    newExample,
+  },
+});
