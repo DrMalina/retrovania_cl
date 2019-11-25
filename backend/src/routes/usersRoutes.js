93ba38691ff5f4cf5e0ad9b04595fa176eb91993
Kamil <37276273+kamilkulik@users.noreply.github.com>
2019-11-25 19:58:33 +0100

    [ND-37] Handling sing-on request

--- backend/src/routes/usersRoutes.js
+++ backend/src/routes/usersRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const signUp = require('../controllers/users/signUp');
+const signOn = require('../controllers/users/signOn');
 
 const router = express.Router();
 
 router.post('/signup', signUp);
+router.post('/', signOn);
 
 module.exports = router;
