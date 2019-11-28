b89635b53b20446a1c1e3eb373a1352b6bcfea26
Vitalik Pylypets <v.pylypets94@gmail.com>
2019-11-28 18:49:05 +0100

    [ND-57] Add offset and page to gamesFetch

--- frontend/src/redux/games/utils.js
+++ frontend/src/redux/games/utils.js
@@ -1,10 +1,11 @@
 import * as actions from 'redux/games/actions';
 import GamesService from 'services/Games';
 
-export const gamesFetch = (limit = 150) => async dispatch => {
+export const gamesFetch = (page = 1, limit = 10) => async dispatch => {
   try {
+    const offset = (page - 1) * limit;
     dispatch(actions.gamesFetchStart());
-    const games = await GamesService.fetchMany(limit);
+    const games = await GamesService.fetch(offset, limit);
     dispatch(actions.gamesFetchSuccess(games));
   } catch (error) {
     dispatch(actions.gamesFetchFailure(error));
