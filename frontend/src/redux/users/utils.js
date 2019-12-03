c8e76c6e72fd9d3ac1449478524ec60b51740cb1
Maciej Malinowski <maciej.malinowski7@gmail.com>
2019-12-03 18:51:36 +0100

    [ND-41] Add/remove user token from local storage when SigningIn/ Signing Up

--- frontend/src/redux/users/utils.js
+++ frontend/src/redux/users/utils.js
@@ -6,7 +6,9 @@ export const signInReq = (formValues, path) => async dispatch => {
     dispatch(actions.signInInit());
     const response = await userAPI.post(path, formValues);
     dispatch(actions.signInSuccess(response.data.user));
+    localStorage.setItem('token', JSON.stringify(response.data.token));
   } catch (error) {
     dispatch(actions.signInFailure(error));
+    localStorage.removeItem('token');
   }
 };
