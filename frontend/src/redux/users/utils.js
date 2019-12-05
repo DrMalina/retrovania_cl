8d327e68da1df4f4141bcd08a271773ea97eb2ec
przem <przemichal@gmail.com>
2019-12-05 17:15:38 +0100

    [ND-72] [HOTFIX] Set global Authorization header

--- frontend/src/redux/users/utils.js
+++ frontend/src/redux/users/utils.js
@@ -1,4 +1,5 @@
 import * as actions from 'redux/users/actions';
+import axios from 'axios';
 import User from 'services/Users';
 
 export const signInReq = (route, formValues) => async dispatch => {
@@ -7,9 +8,11 @@ export const signInReq = (route, formValues) => async dispatch => {
     const response = await User.authenticate(route, formValues);
     dispatch(actions.signInSuccess(response.data.user));
     localStorage.setItem('token', JSON.stringify(response.data.token));
+    axios.defaults.headers.common.Authorization = `Bearer ${ response.data.token }`;
   } catch (error) {
     dispatch(actions.signInFailure(error));
     if (localStorage.token) localStorage.removeItem('token');
+    delete axios.defaults.headers.common.Authorization;
   }
 };
 
@@ -19,8 +22,10 @@ export const deauthenticate = () => async dispatch => {
     const response = await User.deauthenticate();
     dispatch(actions.signOutSuccess(response.data.user));
     if (localStorage.token) localStorage.removeItem('token');
+    delete axios.defaults.headers.common.Authorization;
   } catch (error) {
     dispatch(actions.signOutFailure(error));
     if (localStorage.token) localStorage.removeItem('token');
+    delete axios.defaults.headers.common.Authorization;
   }
 };
