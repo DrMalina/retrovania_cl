9f146086d9782dd28415c2adec91a1f972925cc5
Vitalik Pylypets <v.pylypets94@gmail.com>
2019-12-08 15:18:42 +0100

    [ND-57] Add react fragment to GamesList

--- frontend/src/components/GamesList/GamesList.js
+++ frontend/src/components/GamesList/GamesList.js
@@ -5,19 +5,20 @@ import { Pagination } from '../Pagination/Pagination';
 import * as S from './GamesList.styles';
 
 const GamesList = ({ games, gamesFetch, total }) => (
-  <S.GamesListWrapper>
-    {games.map(({ _id, genres, name, summary }) => (
-      <GamesListItem
-        genres={genres}
-        id={_id}
-        key={_id}
-        name={name}
-        summary={summary}
-      />
-    ))}
-
+  <>
+    <S.GamesListWrapper>
+      {games.map(({ _id, genres, name, summary }) => (
+        <GamesListItem
+          genres={genres}
+          id={_id}
+          key={_id}
+          name={name}
+          summary={summary}
+        />
+      ))}
+    </S.GamesListWrapper>
     <Pagination pageCount={Math.ceil(total / 10)} onClick={gamesFetch} />
-  </S.GamesListWrapper>
+  </>
 );
 
 export { GamesList };
