542aac5271f6e5e2ef8f3d91b1106eaff5d82eb6
Maciej Malinowski <maciej.malinowski7@gmail.com>
2019-12-16 12:18:39 +0100

    [ND-82] Return games to FE based on search query

--- backend/src/controllers/games/getGames.js
+++ backend/src/controllers/games/getGames.js
@@ -2,12 +2,25 @@ const { isInt } = require('validator');
 
 const Game = require('models/Game');
 
+function convertQueryToRegex(query) {
+  //only accept LETTERS, NUMBERS, SPACES
+  if (!query.match(/^[A-Za-z0-9 ]*$/g)) return null;
+  return new RegExp(`${query.split(' ').join('|')}`, 'gi');
+}
+
 async function getGames(req, res, next) {
   try {
-    const { limit = '15', page = '1' } = req.query;
+    const { limit = '15', page = '1', query = '' } = req.query;
+    const totalNumGames = await Game.countDocuments();
 
-    if (isInt(limit, { min: 1 }) && isInt(page, { min: 1 })) {
-      const games = await Game.paginate({}, { limit: +limit, page: +page });
+    if (
+      isInt(limit, { min: 1 }) &&
+      isInt(page, { min: 1, max: Math.ceil(totalNumGames / limit) })
+    ) {
+      const games = await Game.paginate(
+        { name: convertQueryToRegex(query) },
+        { limit: +limit, page: +page },
+      );
       return res.status(200).json(games);
     }
 
