9900a764ff2f210746911e5aade2238191637859
Dominika Słowik <dommie73@gmail.com>
2019-12-18 19:48:20 +0100

    [ND-63] Style `Cart` component

--- frontend/src/components/Cart/Cart.js
+++ frontend/src/components/Cart/Cart.js
@@ -1,43 +1,55 @@
 import React from 'react';
 
-import { Button } from 'components/Button';
 import { DateEditor } from 'components/DateEditor';
-import { Link } from 'components/Link';
 
 import * as S from './Cart.styles';
 
 const Cart = ({ cart, removeItem }) =>
   cart && cart.length ? (
-    <S.CartTable>
-      <S.CartTableHead>
-        <tr>
-          <td>Game name</td>
-          <td>Date from</td>
-          <td>Date to</td>
-          <td>Actions</td>
-        </tr>
-      </S.CartTableHead>
-      <tbody>
-        {cart.map(game => (
-          <tr key={game._id}>
-            <td>
-              <Link to={`/games/${game._id}`}>{game.name}</Link>
-            </td>
-            <td>{new Date(game.from).toLocaleDateString()}</td>
-            <td>{new Date(game.to).toLocaleDateString()}</td>
-            <td>
-              <Button onClick={() => removeItem(game._id)}>Delete</Button>
-              <DateEditor
-                activator={({ setIsOpened }) => (
-                  <Button onClick={() => setIsOpened(true)}>Edit</Button>
-                )}
-                data={game}
-              />
-            </td>
+    <S.CartWrapper>
+      <S.CartTable>
+        <S.CartTableHead>
+          <tr>
+            <th>Game name</th>
+            <th>Rental from</th>
+            <th>Rental to</th>
+            <th>Actions</th>
           </tr>
-        ))}
-      </tbody>
-    </S.CartTable>
+        </S.CartTableHead>
+        <S.CartTableBody>
+          {cart.map(game => (
+            <tr key={game._id}>
+              <td>
+                <S.CartLink to={`/games/${game._id}`}>{game.name}</S.CartLink>
+              </td>
+              <td>{new Date(game.from).toLocaleDateString()}</td>
+              <td>{new Date(game.to).toLocaleDateString()}</td>
+              <td>
+                <S.CartActionButton onClick={() => removeItem(game._id)}>
+                  <span>Delete</span>
+                  <S.TrashIcon />
+                </S.CartActionButton>
+                <DateEditor
+                  activator={({ setIsOpened }) => (
+                    <S.CartActionButton onClick={() => setIsOpened(true)}>
+                      <span>Edit</span>
+                      <S.PencilIcon />
+                    </S.CartActionButton>
+                  )}
+                  data={game}
+                />
+              </td>
+            </tr>
+          ))}
+        </S.CartTableBody>
+      </S.CartTable>
+      <S.CartCheckout>
+        <p>
+          Games in cart: <S.Highlight>{cart.length}</S.Highlight>
+        </p>
+        <S.CartButton>Order</S.CartButton>
+      </S.CartCheckout>
+    </S.CartWrapper>
   ) : (
     <p>You haven't added any products to your cart yet.</p>
   );
--- frontend/src/components/Cart/Cart.styles.js
+++ frontend/src/components/Cart/Cart.styles.js
@@ -1,22 +1,105 @@
 import styled from 'styled-components';
 
+import { Button } from 'components/Button';
+import { Link } from 'components/Link';
+import { ReactComponent as Pencil } from 'assets/icons/pencil.svg';
+import { ReactComponent as Trash } from 'assets/icons/trash.svg';
+
 import { colors, typography } from 'utils';
 
-const { text } = colors;
+const { input, menu, text } = colors;
 const { sizes, weights } = typography;
+
+const CartActionButton = styled.button`
+  border: none;
+  background: none;
+  cursor: pointer;
+  margin: 0 10px;
+
+  & span {
+    clip: rect(1px, 1px, 1px, 1px);
+    position: absolute !important;
+  }
+
+  & svg {
+    height: 30px;
+    width: 30px;
+  }
+`;
+
+const CartButton = styled(Button)`
+  font-size: ${sizes.tiny};
+`;
+
+const CartCheckout = styled.div`
+  align-self: flex-end;
+  font-size: ${sizes.normal};
+  text-align: right;
+`;
+
+const CartLink = styled(Link)`
+  font-size: inherit;
+`;
 
 const CartTable = styled.table`
   border: 3px solid ${text.secondary.hex};
   border-radius: 10px;
+  border-spacing: 0;
   box-shadow: 0 0 10px -1px ${text.secondary.hex},
     0 0 7px -1px ${text.secondary.hex} inset;
   font-size: ${sizes.large};
-  padding: 40px;
+  margin: 20px 0;
+  overflow: hidden;
+
+  & td,
+  & th {
+    padding: 20px 40px;
+  }
+`;
+
+const CartTableBody = styled.tbody`
+  font-size: ${sizes.normal};
+
+  & td:last-child {
+    text-align: center;
+  }
 `;
 
 const CartTableHead = styled.thead`
-  font-weight: ${weights.medium};
+  background: rgba(${menu.shadow.rgb}, 0.5);
   text-transform: uppercase;
 `;
 
-export { CartTable, CartTableHead };
+const CartWrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  margin-bottom: 80px;
+`;
+
+const Highlight = styled.span`
+  color: ${text.tertiary.hex};
+  font-weight: ${weights.bold};
+  margin: 0 5px;
+`;
+
+const PencilIcon = styled(Pencil)`
+  fill: ${input.focus.hex};
+`;
+
+const TrashIcon = styled(Trash)`
+  fill: ${input.error.hex};
+`;
+
+export {
+  CartActionButton,
+  CartButton,
+  CartCheckout,
+  CartLink,
+  CartTable,
+  CartTableBody,
+  CartTableHead,
+  CartWrapper,
+  Highlight,
+  PencilIcon,
+  TrashIcon
+};
